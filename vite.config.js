45f305aec2ad03617afa1b3163e69f6aec59275a
pamesta <pamestapinigine@gmail.com>
2024-04-16 23:59:00 +0300

    add laravel/ui and fix vite and controller and middleware

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/assets/sass/app.scss",
+                "resources/assets/js/app.js",
+            ],
             refresh: true,
         }),
     ],
