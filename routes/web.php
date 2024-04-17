bca58697b6427549459f9543f09a283fd29792e1
pamesta <pamestapinigine@gmail.com>
2024-04-17 15:24:41 +0300

    oops deleted too much

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,7 @@ Route::get('login', [LoginController::class, 'showLoginForm'])->name('login.form
 Route::post('login', [LoginController::class, 'login'])->name('login');
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
+// Route::get('/admin', function () {
+//     return view('admin');
+// });
+
