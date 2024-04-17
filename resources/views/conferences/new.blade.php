97c40dee71db53b56c6cbad8ef8944c7a36bded4
pamesta <pamestapinigine@gmail.com>
2024-04-17 20:36:53 +0300

    add create conference functionality

--- resources/views/conferences/new.blade.php
+++ resources/views/conferences/new.blade.php
@@ -5,16 +5,16 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Edit conference') }}</div>
+                    <div class="card-header">{{ __('New conference') }}</div>
 
                     @if (@session('status'))
                         <div class="mx-auto bg-success text-white px-3 w-100">
-                            {{ session('status') . ' ' . $conference->title }}
+                            {{ session('status') }}
                         </div>
                     @endif
 
                     <div class="card-body">
-                        <form method="POST" action="{{ route('conferences/edit', $conference->id) }}">
+                        <form method="POST" action="{{ route('conferences/new') }}">
                             @csrf
 
                             <div class="row mb-3">
@@ -23,7 +23,7 @@
 
                                 <div class="col-md-10">
                                     <input id="title" type="text" class="form-control" name="title"
-                                        value="{{ old('title', $conference->title) }}">
+                                        value="{{ old('title') }}">
 
                                     @error('title')
                                         <div class="text-danger">{{ $message }}</div>
@@ -36,7 +36,7 @@
                                     class="col-md-2 col-form-label text-md-end">{{ __('Description') }}</label>
 
                                 <div class="col-md-10">
-                                    <textarea id="description" type="text" class="form-control" name="description" rows="6">{{ old('description', $conference->description) }}</textarea>
+                                    <textarea id="description" type="text" class="form-control" name="description" rows="6">{{ old('description') }}</textarea>
 
                                     @error('description')
                                         <div class="text-danger">{{ $message }}</div>
@@ -50,7 +50,7 @@
 
                                 <div class="col-md-10">
                                     <input id="date" type="text" class="form-control" name="date"
-                                        value="{{ old('date', $conference->date) }}">
+                                        value="{{ old('date') }}">
 
                                     @error('date')
                                         <div class="text-danger">{{ $message }}</div>
@@ -64,7 +64,7 @@
 
                                 <div class="col-md-10">
                                     <input id="adress" type="text" class="form-control" name="adress"
-                                        value="{{ old('adress', $conference->adress) }}">
+                                        value="{{ old('adress') }}">
 
                                     @error('adress')
                                         <div class="text-danger">{{ $message }}</div>
@@ -76,7 +76,7 @@
                             <div class="row mb-0">
                                 <div class="col-md-6 offset-md-5">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Update') }}
+                                        {{ __('Create') }}
                                     </button>
                                 </div>
                             </div>
